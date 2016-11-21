6700935fa4db1dea3889701d33c83aac666d5ca4
Pósa Márton <posa.marci@gmail.com>
2016-11-21 12:44:08 +0100

    removed remnants of moved methods

--- src/garden/Garden.java
+++ src/garden/Garden.java
@@ -9,14 +9,6 @@ import java.util.Random;
  * Created by posam on 2016-11-21.
  */
 public class Garden<E> extends ArrayList<Plantable> implements Plantable {
-    static ArrayList<String> colors = new ArrayList<>(Arrays.asList("white", "blue", "red", "purple", "black", "green", "yellow"));
-
-    static Random rand = new Random();
-    static <T> T getRandomItem(List<T> list) {
-        return list.get(rand.nextInt(list.size()));
-    }
-
-
     public double water(double amount) {
         System.out.printf("\nWatering with %f.\n", amount);
         int needy = 0;
